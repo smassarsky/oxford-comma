c4e7ee5cbc2b6ba4963b85acb5e29f73f8e1ea04
smassarsky <seth.massarsky@gmail.com>
2020-09-30 14:49:33 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  case array.length
+  when 0
+    ""
+  when 1
+    array[0]
+  when 2
+    "#{array[0]} and #{array[1]}"
+  else
+    array[array.length - 1] = "and #{array[array.length - 1]}"
+    array.join(", ")
+  end
 end
